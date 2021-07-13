cd130b613c04f48cf20fe4b54538065a4f7d499c
FGlider <83852473+hungyifang@users.noreply.github.com>
2021-07-13 19:05:11 +0800

    建立api/event.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require("./routes/index");
 // var usersRouter = require('./routes/users');
 
 var usersAPI = require("./api/users");
+var eventAPI = require("./api/event");
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersAPI);
+app.use("/event", eventAPI);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
