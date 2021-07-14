21141561001b1b2c4eface17d3a11edad9e58080
Lzmxya <lzmxya@gmail.com>
2021-07-14 12:07:02 +0800

    新增 Prettier 設定檔，並格式化現有文件

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var cors = require("cors");
 var cookieParser = require("cookie-parser");
 var expressSession = require("express-session");
 var logger = require("morgan");
-// var bodyparser = require("body-parser");
 
 var indexRouter = require("./routes/index");
-// var usersRouter = require('./routes/users');
-
 var apiRouter = require("./routes/api");
-// var usersAPI = require("./api/users");
+
 
 var app = express();
 
@@ -27,31 +24,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-    expressSession({
-        secret: process.env.SESSION_SECRET,
-        saveUninitialized: false,
-        resave: false,
-    })
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
 );
 
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
-// app.use("/users", usersAPI);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
--- utils/db.js
+++ utils/db.js
@@ -4,14 +4,14 @@ const mysql = require("mysql2");
 
 // 建立資料庫連線
 const connection = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    // dateStrings: true,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // dateStrings: true,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
 
 conn = mysql.createPool(connection).promise();
