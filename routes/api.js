6346ebd3a3a56e1da77f45b12d8895a9b32335df
Lzmxya <lzmxya@gmail.com>
2021-08-02 16:09:49 +0800

    更新 api/event

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 var authAPI = require("../api/auth");
+
+// Yuna
 var usersAPI = require("../api/users");
 var pointAPI = require("../api/points");
 var ordersAPI = require("../api/orders");
@@ -9,6 +11,9 @@ var favAPI = require("../api/fav");
 var commentAPI = require("../api/comment");
 var orderviewAPI = require("../api/orderview");
 var avatarAPI = require("../api/avatar");
+
+// Fang
+var eventAPI = require("../api/event");
 
 router.use("/auth", authAPI);
 router.use("/users", usersAPI);
@@ -18,5 +23,6 @@ router.use("/fav", favAPI);
 router.use("/comment", commentAPI);
 router.use("/orderview", orderviewAPI);
 router.use("/avatar", avatarAPI);
+router.use("/event", eventAPI);
 
 module.exports = router;
