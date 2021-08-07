0551253c7bfc69bbd0ddb2e98544228dd2a0127e
HOOGA0828 <popo51102@gmail.com>
2021-08-08 05:19:01 +0800

    新增給截圖用的查詢

--- api/items.js
+++ api/items.js
@@ -16,6 +16,13 @@ router.get("/item", async (req, res, next) => {
   res.json(result[0]);
 });
 
+router.get("/orderedAndDetail", async (req, res, next) => {
+  let result = await conn.query(
+    `SELECT * FROM  ordered_detail `
+  );
+  res.json(result[0]);
+});
+
 // `SELECT * FROM items, items_cat, trips WHERE items.i_id = items_cat.i_id AND items_cat.i_id = trips.as_a_item`
 
 module.exports = router;
