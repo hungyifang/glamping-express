191bf4f250922211385e7e2429c08a00b83ce65b
Lzmxya <lzmxya@gmail.com>
2021-08-10 23:05:12 +0800

    新增註冊、兩步驟登入

--- api/auth.js
+++ api/auth.js
@@ -1,9 +1,50 @@
 const express = require("express");
+const conn = require("../utils/db");
 const router = express.Router();
 const usersController = require("../controllers/users");
+const twofactor = require("node-2fa");
 
 router.get("/logout", usersController.logout);
 router.get("/check", usersController.isLoggedIn);
 router.post("/login", usersController.login);
+
+// router.get("/newtotp", (req, res) => {
+//   const newSecret = twofactor.generateSecret({
+//     name: "山角行",
+//     account: req.body.username,
+//   });
+//   console.log(newSecret);
+//   res.json(newSecret);
+// });
+
+router.post("/signup", async (req, res) => {
+  const row = await conn.query(
+    `SELECT * FROM users WHERE username="${req.body.username}"`
+  );
+  console.log(row[0].length);
+  if (row[0].length === 0) {
+    console.log("可註冊");
+    // CREATE A TOTP CODE
+    const totp = twofactor.generateSecret({
+      name: "山角行",
+      account: req.body.username,
+    });
+    // CREATE A USER
+    await conn.query(
+      `INSERT INTO users (username, password, totp)  VALUES (?)`,
+      [[req.body.username, req.body.password, totp.secret]]
+    );
+    res.json({
+      status: 1,
+      username: req.body.username,
+      totp_uri: totp.uri,
+      totp_qr: totp.qr,
+      msg: "註冊成功",
+    });
+  } else {
+    console.log("使用者名稱已被註冊");
+    res.json({ status: 0, msg: "使用者名稱已被註冊" });
+  }
+});
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,29 @@
 const passport = require("passport");
+const twofactor = require("node-2fa");
 
 module.exports.login = async (req, res, next) => {
-  //console.log('Inside POST /login callback')
   console.log(req.body);
   passport.authenticate("local", (err, user, info) => {
     req.login(user, (err) => {
-      //console.log(req.session.passport, req.user)
-      if (req.user) return res.json({ status: 1, u_id: req.user.u_id });
-      else return res.json({ status: 0, msg: "帳號或密碼錯誤" });
+      if (req.user && req.user.totp === null) {
+        return res.json({ status: 1, u_id: req.user.u_id });
+      }
+      if (req.user && req.user.totp !== null) {
+        const delta = twofactor.verifyToken(req.user.totp, req.body.totp);
+        console.log(delta);
+        if (delta === null) {
+          console.log("兩步驟驗證碼錯誤");
+          req.logout();
+          return res.json({ status: 0, msg: "兩步驟驗證碼錯誤" });
+        }
+        if (delta.delta === 0) {
+          return res.json({ status: 1, u_id: req.user.u_id });
+        }
+      }
+      if (!req.user) {
+        req.logout();
+        return res.json({ status: 0, msg: "帳號或密碼錯誤" });
+      }
     });
   })(req, res, next);
 };
